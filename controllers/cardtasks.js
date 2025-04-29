bf970515f8ddf2d838456b111ccaa27220d306b3
isaias <isaias.gaona@zentro.com.mx>
2025-04-29 15:44:06 -0600

    modificaciones

--- controllers/cardtasks.js
+++ controllers/cardtasks.js
@@ -1,45 +1,45 @@
 const CardTasks = require("../models/cardtask");
 
 const prueba = async (req, res) => {
-  return res.status(200).send({
-    status: "success",
-    message: "Prueba desde controlador task",
-  });
+    return res.status(200).send({
+        status: "success",
+        message: "Prueba desde controlador task",
+    });
 };
 
 const save = async (req, res) => {
-  let params = req.body;
+    let params = req.body;
 
-  try {
-    if (!params.title) {
-      throw new Error("El titulo es obligatorio");
+    try {
+        if (!params.title) {
+            throw new Error("El titulo es obligatorio");
+        }
+    } catch (error) {
+        return res.status(400).send({
+            status: "error",
+            message: error.message,
+        });
     }
-  } catch (error) {
-    return res.status(400).send({
-      status: "error",
-      message: error.message,
-    });
-  }
 
-  // Crea el objeto
-  const cardTasksToSave = new CardTasks(params);
+    // Crea el objeto
+    const cardTasksToSave = new CardTasks(params);
 
-  // Guardar en la base de datos
-  try {
-    const cardTasksStored = await cardTasksToSave.save();
-    return res.status(200).send({
-      status: "success",
-      cardtasks: cardTasksStored,
-    });
-  } catch (error) {
-    return res.status(500).send({
-      status: "error",
-      message: "Error al guardar la cardtasks",
-    });
-  }
+    // Guardar en la base de datos
+    try {
+        const cardTasksStored = await cardTasksToSave.save();
+        return res.status(200).send({
+            status: "success",
+            cardtasks: cardTasksStored,
+        });
+    } catch (error) {
+        return res.status(500).send({
+            status: "error",
+            message: "Error al guardar la cardtasks",
+        });
+    }
 };
 
-const update = async (req,res) => {
+const update = async (req, res) => {
     // Recoger el id de la url
     let cardTasksId = req.params.id;
 
@@ -62,7 +62,7 @@ const update = async (req,res) => {
 
     // Actualizar el cardTasks en la base de datos
     try {
-        const cardTasksUpdated = await CardTasks.findByIdAndUpdate(cardTasksId, params, {new: true});
+        const cardTasksUpdated = await CardTasks.findByIdAndUpdate(cardTasksId, params, { new: true });
         if (!cardTasksUpdated) {
             return res.status(404).send({
                 status: 'error',
@@ -81,26 +81,36 @@ const update = async (req,res) => {
     }
 }
 
-const list = async(req,res) => {
+const list = async (req, res) => {
     try {
         const cardsTasksList = await CardTasks.find();
 
-        if(!cardsTasksList){
+        if (!cardsTasksList) {
             return res.status(404).send({
                 status: 500,
                 message: 'No hay cardtasks por obtener'
-            })   
+            })
         }
 
-        return res.status(404).send({
-            status: 'success',
-            data: cardsTasksList
+        const formattedCardsTasksList = cardsTasksList.map(card => ({
+            title: card.title,
+            description: card.description,
+            tasks: Array.isArray(card.tasks) ? card.tasks.map(task => ({
+                description: task.description,
+                isCompleted: task.isCompleted
+            })) : []
+        }));
+
+        return res.status(200).send({
+            status: 200,
+            cardsTasks: formattedCardsTasksList
         });
 
     } catch (error) {
         return res.status(500).send({
             status: 500,
-            message: 'Error al obtener la lista de cardstasks'
+            message: 'Error al obtener la lista de cardstasks',
+            error
         })
     }
 }
@@ -121,8 +131,8 @@ const remove = async (req, res) => {
     try {
 
         const cardTasks = await CardTasks.findByIdAndDelete(cardTasksId)
-        
-        if(!cardTasks){
+
+        if (!cardTasks) {
             return res.status(200).send({
                 status: 'error',
                 message: 'Error al eliminar la card'
@@ -145,9 +155,9 @@ const remove = async (req, res) => {
 }
 
 module.exports = {
-  prueba,
-  save,
-  update,
-  list,
-  remove
+    prueba,
+    save,
+    update,
+    list,
+    remove
 };
