72d02eac1396dda854075f01cb9a03656a85fc7a
isaias <isgb_codigo@hotmail.com>
2025-05-02 00:30:38 -0600

    base datos mongo

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connection = async () => {
 
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/app_todolist')
+    await mongoose.connect(process.env.MONGO_URI)
     console.log('Conexión a la base de datos')
   } catch (error) {
      throw new Error("Error al conectarse a la base de datos")
